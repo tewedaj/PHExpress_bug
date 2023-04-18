8f50bfdd91efd5076ced7b1efaf3a25130fa10b9
tewedaj <tewedaj13@gmail.com>
2023-04-18 06:17:05 +0300

    * Fixeda bug to write on to the route folder to the model name Added the code generater functions

--- src/controllers/crud.js
+++ src/controllers/crud.js
@@ -1,5 +1,6 @@
 
 const { posix } = require('path');
+const { select, selectBy, selectById } = require('../utils/endpoints/selectEndpoint');
 
 //todo: Take the model the command is run on
 //todo: Creacte a route file with the model name
@@ -10,15 +11,29 @@ const { posix } = require('path');
 		var path = vscode.window.activeTextEditor.document.uri._fsPath.toString();
 		var folderUri = vscode.workspace.workspaceFolders[0].uri;
 		var modelName = vscode.window.activeTextEditor.document.uri.toString().split("/")[vscode.window.activeTextEditor.document.uri.toString().split("/").length - 1].split(".php")[0];
-
          
-        var writeWord = Buffer.from(pageContent,'utf8');
-		// var folderUri = vscode.workspace.workspaceFolders[0].uri;
-		// var fileLocation = folderUri.with({path: posix.join(folderUri.split("%20").join(" ").split("c%3a").join("C://")+"/routes/", modelName+".php") });
-        // console.log("uuuuuuuuuuu: ", folderUri.split("%20").join(" ").split("c%3a").join("C://")+"../routes/");
-		await vscode.workspace.fs.writeFile( vscode.window.activeTextEditor.document.uri,writeWord);
+        var codeBlock = `<?php
+        
+        //auto generated file \n 
+        
+        ${select(modelName)} \n \n 
+        ${selectById(modelName)} \n \n
+        
+        `;
 
-        return JSON.stringify("{}");
+        //'/private\s+(\w+)\s+\$(\w+)\s*;\s*\/\/size:\s*(\d+)?/'
+
+        var buffredCodeBlock = Buffer.from(codeBlock,'utf8');
+
+        
+        var folderUri = vscode.workspace.workspaceFolders[0].uri;
+        var activeEditorUri = vscode.window.activeTextEditor.document.uri.toString();
+			activeEditorUri = activeEditorUri.replace(activeEditorUri.split("/")[activeEditorUri.split("/").length-1],"");
+			activeEditorUri = activeEditorUri.replace(activeEditorUri.split("/")[0],"");
+		var fileLocation = folderUri.with({path: posix.join(activeEditorUri.split("%20").join(" ").split("c%3A").join("C://")+"../routes/", modelName+'.php') });
+		await vscode.workspace.fs.writeFile( fileLocation,buffredCodeBlock);
+
+        return JSON.stringify(fileLocation);
         console.log("Title: ", pageContent);
         console.log("Path: ", path);
         console.log("folderUri: ", folderUri);
