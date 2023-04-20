e7dcfa99107693ce2b2e86ea4f6d29a280333df6
tewedaj <tewedaj13@gmail.com>
2023-04-20 23:46:24 +0300

    * Added API routers for PATCH and DELETE, * Updated POST API router

--- util/phexpress.php
+++ util/phexpress.php
@@ -1,8 +1,9 @@
 <?php
 /*
-This PHP file handles all the routing interfaces, with support for HTTP methods POST, GET, PATCH, and DELETE.
-The code is designed to resemble the syntax of Node.js as closely as possible.
-*/
+This PHP script implements a routing mechanism that supports HTTP methods such as POST, GET, PATCH, and DELETE.
+The code is structured to closely resemble the syntax of Node.js, providing developers with a familiar programming paradigm. Through this script,
+web developers can easily define and manage routes in their web applications. By utilizing a syntax that is similar to Node.js, the script simplifies the process of building web applications in PHP,
+thereby improving the efficiency and productivity of web development. */
 
 class phexpress
 {
@@ -25,7 +26,7 @@ class phexpress
         $routes = "/".$this->parent.$routes;
         $url = $this->requests["url"];
 
-        if (!($this->requests["method"] == "POST")) {
+        if (($this->requests["method"] == "GET")) {
 
             $path = parse_url($url, PHP_URL_PATH);
             $path3 = explode(rtrim($routes,"/"), $path);
@@ -74,27 +75,152 @@ class phexpress
     }
 
 
-
     public function Post(String $routes, $callBack)
     {
+        $routes = "/".$this->parent.$routes;
+        $url = $this->requests["url"];
 
-        // Check if HTTP Request type
-        // pass if it is POST
-        if ($this->requests["method"] == "POST") {
-            
-            $url = $_SERVER["REQUEST_URI"];
+        if (($this->requests["method"] == "POST")) {
+
             $path = parse_url($url, PHP_URL_PATH);
-            $path3 = explode($routes, $path);
-
+            $path3 = explode(rtrim($routes,"/"), $path);
             if (count($path3) > 1) {
-                if ($path3[1] == "") {
+                if ($path3[1] == "" || $path3[1] == "/") {
                     $response = new Response();
                     $callBack($this->requests, $response);
                     exit;
                 }
             } else {
-                return false;
+
+                //generate a specific request based on the route given
+                $pattern =   getPathRegex($routes);
+                $pattern = str_replace('\(', '(', $pattern);
+                $variables = explode(":", $routes);
+
+                if (count($variables) > 1) {
+                    $matches = array();
+                    $finalUri = str_replace("PHExpress/", "", $path3[0]);
+                    $finalUri = rtrim($finalUri, "/");
+
+
+                    if (preg_match($pattern, $finalUri, $matches)) {
+                        $keyMatch = array();
+
+                        preg_match($pattern, str_replace(":", "", $routes), $keyMatch);
+
+
+                        for($x = 0 ; $x < count($matches); $x++){
+                            $this->requests["params"][$keyMatch[$x]] = $matches[$x];
+                            
+                        }
+
+                        $response = new Response();
+                        $callBack($this->requests, $response);
+                        exit;
+                    }
+                }
             }
         }
     }
+
+    public function Patch(String $routes, $callBack)
+    {
+        $routes = "/".$this->parent.$routes;
+        $url = $this->requests["url"];
+
+        if (($this->requests["method"] == "PATCH")) {
+
+            $path = parse_url($url, PHP_URL_PATH);
+            $path3 = explode(rtrim($routes,"/"), $path);
+            if (count($path3) > 1) {
+                if ($path3[1] == "" || $path3[1] == "/") {
+                    $response = new Response();
+                    $callBack($this->requests, $response);
+                    exit;
+                }
+            } else {
+
+                //generate a specific request based on the route given
+                $pattern =   getPathRegex($routes);
+                $pattern = str_replace('\(', '(', $pattern);
+                $variables = explode(":", $routes);
+
+                if (count($variables) > 1) {
+                    $matches = array();
+                    $finalUri = str_replace("PHExpress/", "", $path3[0]);
+                    $finalUri = rtrim($finalUri, "/");
+
+
+                    if (preg_match($pattern, $finalUri, $matches)) {
+                        $keyMatch = array();
+
+                        preg_match($pattern, str_replace(":", "", $routes), $keyMatch);
+
+
+                        for($x = 0 ; $x < count($matches); $x++){
+                            $this->requests["params"][$keyMatch[$x]] = $matches[$x];
+                            
+                        }
+
+                        $response = new Response();
+                        $callBack($this->requests, $response);
+                        exit;
+                    }
+                }
+            }
+        }
+    }
+
+
+    public function Delete(String $routes, $callBack)
+    {
+        $routes = "/".$this->parent.$routes;
+        $url = $this->requests["url"];
+
+        if (($this->requests["method"] == "DELETE")) {
+
+            $path = parse_url($url, PHP_URL_PATH);
+            $path3 = explode(rtrim($routes,"/"), $path);
+            if (count($path3) > 1) {
+                if ($path3[1] == "" || $path3[1] == "/") {
+                    $response = new Response();
+                    $callBack($this->requests, $response);
+                    exit;
+                }
+            } else {
+
+                //generate a specific request based on the route given
+                $pattern =   getPathRegex($routes);
+                $pattern = str_replace('\(', '(', $pattern);
+                $variables = explode(":", $routes);
+
+                if (count($variables) > 1) {
+                    $matches = array();
+                    $finalUri = str_replace("PHExpress/", "", $path3[0]);
+                    $finalUri = rtrim($finalUri, "/");
+
+
+                    if (preg_match($pattern, $finalUri, $matches)) {
+                        $keyMatch = array();
+
+                        preg_match($pattern, str_replace(":", "", $routes), $keyMatch);
+
+
+                        for($x = 0 ; $x < count($matches); $x++){
+                            $this->requests["params"][$keyMatch[$x]] = $matches[$x];
+                            
+                        }
+
+                        $response = new Response();
+                        $callBack($this->requests, $response);
+                        exit;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+  
 }
